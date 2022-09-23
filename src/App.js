fd15ada2f2621b35a4651f50285690ce993540f2
yiinuma <65111001+yiinuma@users.noreply.github.com>
2022-09-23 17:37:06 +0900

    add: all

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import styles from './App.module.css';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import Auth from './components/Auth';
-import MainPage from './components/MainPage';
-// import StateContextProvider from './context/StateContext';
+import React from "react";
+import styles from "./App.module.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Auth from "./components/Auth";
+import MainPage from "./components/MainPage";
+import StateContextProvider from "./context/StateContext";
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:8000/graphql/',
+  uri: "http://127.0.0.1:8000/graphql/",
   headers: {
-    authorization: localStorage.getItem('token') ? `JWT ${localStorage.getItem('token')}` : '',
+    authorization: localStorage.getItem("token")
+      ? `JWT ${localStorage.getItem("token")}`
+      : "",
   },
   cache: new InMemoryCache(),
 });
 function App() {
   return (
     <ApolloProvider client={client}>
-      {/* <StateContextProvider> */}
-      <div className={styles.app}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Auth} />
-            <Route exact path="/employees" component={MainPage} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-      {/* </StateContextProvider> */}
+      <StateContextProvider>
+        <div className={styles.app}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Auth} />
+              <Route exact path="/employees" component={MainPage} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </StateContextProvider>
     </ApolloProvider>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import styles from './Auth.module.css';
-import { GET_TOKEN, CREATE_USER } from '../queries';
-import { useMutation } from '@apollo/client';
-import jwtDecode from 'jwt-decode';
-import FlipCameraAndroidIcon from '@material-ui/icons/FlipCameraAndroid';
+import React, { useState, useEffect } from "react";
+import styles from "./Auth.module.css";
+import { GET_TOKEN, CREATE_USER } from "../queries";
+import { useMutation } from "@apollo/client";
+import jwtDecode from "jwt-decode";
+import FlipCameraAndroidIcon from "@material-ui/icons/FlipCameraAndroid";
 
 const Auth = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [getToken] = useMutation(GET_TOKEN);
   const [createUser] = useMutation(CREATE_USER);
   const [isLogin, setIsLogin] = useState(true);
@@ -19,8 +19,8 @@ const Auth = () => {
         const result = await getToken({
           variables: { username: username, password: password },
         });
-        localStorage.setItem('token', result.data.tokenAuth.token);
-        result.data.tokenAuth.token && (window.location.href = '/employees');
+        localStorage.setItem("token", result.data.tokenAuth.token);
+        result.data.tokenAuth.token && (window.location.href = "/employees");
       } catch (err) {
         alert(err.message);
       }
@@ -32,8 +32,8 @@ const Auth = () => {
         const result = await getToken({
           variables: { username: username, password: password },
         });
-        localStorage.setItem('token', result.data.tokenAuth.token);
-        result.data.tokenAuth.token && (window.location.href = '/employees');
+        localStorage.setItem("token", result.data.tokenAuth.token);
+        result.data.tokenAuth.token && (window.location.href = "/employees");
       } catch (err) {
         alert(err.message);
       }
@@ -41,12 +41,12 @@ const Auth = () => {
   };
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      const decodedToken = jwtDecode(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      const decodedToken = jwtDecode(localStorage.getItem("token"));
       if (decodedToken.exp * 1000 < Date.now()) {
-        localStorage.removeItem('token');
+        localStorage.removeItem("token");
       } else {
-        window.location.href = '/employees';
+        window.location.href = "/employees";
       }
     }
   }, []);
@@ -56,17 +56,30 @@ const Auth = () => {
       <form onSubmit={authUser}>
         <div className={styles.auth__input}>
           <label>Username: </label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className={styles.auth__input}>
           <label>Password: </label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
-        <button type="submit">{isLogin ? 'Login with JWT' : 'Create new user'}</button>
+        <button type="submit">
+          {isLogin ? "Login with JWT" : "Create new user"}
+        </button>
 
         <div>
-          <FlipCameraAndroidIcon className={styles.auth__toggle} onClick={() => setIsLogin(!isLogin)} />
+          <FlipCameraAndroidIcon
+            className={styles.auth__toggle}
+            onClick={() => setIsLogin(!isLogin)}
+          />
         </div>
       </form>
     </div>
